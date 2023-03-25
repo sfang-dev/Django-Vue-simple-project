f5b41cf1b3fae684cf6e9b292f90377719677cbe
feng-sifang <fsf1999@yahoo.com>
2023-03-25 20:21:28 +0800

    Views, URLs & Templates

---
+++ games/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from games.views import HomeView, MathGameView, AnagramGameView
+
+app_name = "games"
+urlpatterns = [
+    path('', HomeView.as_view(), name="homepage"),
+    path('math-game/', MathGameView.as_view(), name="math-game"),
+    path('anagram-game/', AnagramGameView.as_view(), name="anagram-game")
+]
--- games/views.py
+++ games/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class MathGameView(TemplateView):
+    template_name = "math-game.html"
+
+
+class AnagramGameView(TemplateView):
+    template_name = "anagram-game.html"
