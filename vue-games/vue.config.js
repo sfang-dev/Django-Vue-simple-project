b6d4dc205d08be605b0bc160ee3c3c70c71207d2
feng-sifang <fsf1999@yahoo.com>
2023-03-25 22:49:29 +0800

    Combine Vue & Django

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: 'http://localhost:8080',  // the base URL where your app will be deployed
-  outputDir: '../static/dict',          // the path for where files will be output when the app is built
+  outputDir: '../static/dist',          // the path for where files will be output when the app is built
   indexPath: '../../templates/_base_vue.html', // the path for the generated index file
 
   configureWebpack: {
