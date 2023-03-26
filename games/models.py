827ab5ed45a0be73eee012fd02775e08d364b92b
feng-sifang <fsf1999@yahoo.com>
2023-03-26 12:23:03 +0800

    Add GAME_CHOICES

--- games/models.py
+++ games/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 class GameScore(models.Model):
+    MATH = "MATH"
+    ANAGRAM = "ANAGRAM"
+
+    GAME_CHOICES = [
+        (MATH, "Math Game"),
+        (ANAGRAM, "Anagram Game")
+    ]
+
     user_name = models.TextField()
-    game = models.TextField()
+    game = models.TextField(choices=GAME_CHOICES, default=MATH)
     score = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
