2a9fd8d9713c08bfea2cc15c683224f10b56144c
feng-sifang <fsf1999@yahoo.com>
2023-03-26 15:40:42 +0800

    Add axios & csrf_token

--- vue-games/src/main.js
+++ vue-games/src/main.js
@@ -5,6 +5,10 @@ import App from './App';
 import axios from 'axios';
 import VueAxios from "vue-axios";
 
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+
 const app = createApp(App);
 
 app.use(router);  // tell app to use router
